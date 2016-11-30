3ee3607037bb2c780dc1660b80991d5f2de04187
Gregory Enriquez <gregoryenriquez@gmail.com>
2016-11-29 16:55:36 -0800

    add some imp

--- src/com/panthers/external/WarehouseManager.java
+++ src/com/panthers/external/WarehouseManager.java
@@ -1,8 +1,16 @@
 package com.panthers.external;
 
 import java.util.ArrayList;
+
 import com.panthers.utilities.Warehouse;
 
 public class WarehouseManager {
 	private ArrayList<Warehouse> warehouses;
+	private final int NUM_WAREHOUSES = 5;
+	
+	public WarehouseManager() {
+	  for (int i = 0; i < NUM_WAREHOUSES; i++) {
+	    warehouses.add(new Warehouse());
+	  }
+	}
 }

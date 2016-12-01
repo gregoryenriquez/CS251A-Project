df9a2355f62850f2b27fc4e96028f354f9fa37a8
skycckk <wayne@Waynes-MacBook-Pro.local>
2016-12-01 03:06:59 -0800

    implement depreciation report
    by assuming there is a depreciation ratio for each store

--- src/com/panthers/reports/DepreciationEstimator.java
+++ src/com/panthers/reports/DepreciationEstimator.java
@@ -1,6 +1,12 @@
 package com.panthers.reports;
 
+import com.panthers.orders.Transaction;
+import com.panthers.services.InventoryManager;
+import com.panthers.store.Store;
+import com.panthers.utilities.Date;
 import com.panthers.utilities.Money;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class DepreciationEstimator implements Report {
@@ -16,18 +22,50 @@ public class DepreciationEstimator implements Report {
 	
 	@Override
 	public void printReport() {
-		// TODO Auto-generated method stub
+            // TODO Auto-generated method stub
+            // Deal with UI to display records
 		
 	}
 
 	@Override
 	public void saveReportAsPDF(String filePath) {
-		// TODO Auto-generated method stub
+            // TODO Auto-generated method stub
+            // Bascially, this area should generate PDF with layout of every records.
+            // Then save file to given path
 		
 	}
 	
 	public Money estimateRemainingValue(String UPC) {
-		return null;	
-	}
-	
+            Store store = InventoryManager.getInstance().queryInventory(UPC);
+            // float depreciationRatio = store.getDepreciationRatio();
+            float depreciationRatio = 0.9f;
+            // Money originalPrice = store.getProduct().getUnitPrice();
+            Money originalPrice = new Money(100);
+            double newPriceAmount = originalPrice.getAmount() * depreciationRatio;
+            
+            // Transaction[] allTrans = store.getStatements();
+            Transaction[] allTrans = null;
+            double totalNewAmount = 0;
+            int totalBuyingCount = 0;
+            ArrayList<Transaction> trans = new ArrayList<Transaction>(Arrays.asList(allTrans));
+            for (int i = 0; i < trans.size(); i++) {
+                Transaction t = trans.get(i);
+                // if (t.getType == PURCHASE)
+                //     continue;
+                
+                // Money buyingPrice = t.getOrder().getPrice();
+                // Date buyingDate = t.getOrder().getPurchaseOrderDate();
+                Money buyingPrice = null;
+                Date buyingDate = null;
+                Date currentDate = null;
+                
+                int durationInDays = Date.getDurationInDays(buyingDate, currentDate);
+                int growingSteps = durationInDays % 30;
+                Money currentPrice = new Money(buyingPrice.getAmount() * Math.pow(depreciationRatio, growingSteps));
+
+                totalNewAmount += currentPrice.getAmount();
+                totalBuyingCount++;
+            }
+            return new Money(totalNewAmount / totalBuyingCount);
+	}	
 }

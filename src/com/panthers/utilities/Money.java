53776fba64a6911beee98348b7e43acd312d6525
Shyam Sundar <dssundar@hotmail.com>
2016-12-01 08:10:21 -0800

    Update Money.java

--- src/com/panthers/utilities/Money.java
+++ src/com/panthers/utilities/Money.java
@@ -1,13 +1,53 @@
 package com.panthers.utilities;
 
-public class Money {
-    private double amount;
-    
-    public Money(double amount) {
-        this.amount = amount;
-    }
 
-    public double getAmount() {
-        return amount;
-    }
+public class Money implements Comparable <Money>
+{
+	private int majorAmount ;
+	private int minorAmount;
+	
+	public int getMajorAmount ()
+	{
+		return majorAmount;
+	}
+	
+	public int getMinorAmount ()
+	{
+		return minorAmount;
+	}
+	
+	public Money (int majorAmount, int minorAmount)
+	{
+		this.majorAmount = majorAmount;
+		this.minorAmount = minorAmount;
+	}
+	
+	
+	public Money add (Money other)
+	{
+		int majorAmount = this.majorAmount + other.minorAmount;
+		int minorAmount = this.minorAmount + other.minorAmount;
+		if (minorAmount > 99)
+		{
+			majorAmount = majorAmount + minorAmount / 100;
+			minorAmount = minorAmount % 100;
+		}
+		return new Money (majorAmount, minorAmount);
+	}
+	
+    
+    //implement sub??
+	public Money sub (Money other)
+	{
+		return new Money (this.majorAmount + other.majorAmount, this.minorAmount + other.minorAmount);
+	}
+
+	@Override
+	public int compareTo(Money other) {
+		if ((this.majorAmount == other.majorAmount) && *(this.minorAmount == this.minorAmount))
+			return 1;
+		else
+			return 0;
+	}
+
 }

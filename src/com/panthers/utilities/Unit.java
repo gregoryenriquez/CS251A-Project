fe13abcfd8cb4f8b6707cde56127c789abaa4007
Shyam Sundar <dssundar@hotmail.com>
2016-12-01 05:30:19 -0800

    Update Unit.java

--- src/com/panthers/utilities/Unit.java
+++ src/com/panthers/utilities/Unit.java
@@ -1,1 +1,27 @@
+package com.panthers.utilities;
 
+
+public class Unit 
+{
+	private String name;
+	public static Unit KILO;
+	public static Unit GRAM;
+	public static Unit POUND;
+	public static Unit OUNCE;
+	public static Unit GALLON;
+	public static Unit LITRE;
+	public static Unit COUNT;
+	
+	public String getUnintName()
+	{
+		return name;
+	}
+	
+	public void setUnitName(String name)
+	{
+		this.name = name;
+	}
+	
+	
+	
+}

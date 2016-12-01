2f4468eca98f6dc97966068bf98a00ce01daa39d
Gregory Enriquez <gregoryenriquez@gmail.com>
2016-12-01 02:11:40 -0800

    basic store, quantity

--- src/com/panthers/store/Store.java
+++ src/com/panthers/store/Store.java
@@ -1,5 +1,86 @@
 package com.panthers.store;
 
-public class Store {
+import java.util.ArrayList;
 
+import com.panthers.orders.OrderPoint;
+import com.panthers.orders.Transaction;
+import com.panthers.utilities.Quantity;
+import com.panthers.utilities.Unit;
+
+public class Store {
+	private String storeId;
+	private Quantity productQuantity;
+	private Quantity reservedQuantity;
+	private ArrayList<Transaction> records;
+	private OrderPoint orderPoint;
+	private Product product;
+	
+	public enum STORE_STATUS {
+		LOW_STOCK,
+		OUT_OF_STOCK,
+		AVAILABLE
+	}
+	
+	public Store(String id, Product p, OrderPoint op) {
+		this.storeId = id;
+		this.product = p;
+		this.productQuantity = new Quantity(0, Unit.COUNT);
+		this.reservedQuantity = new Quantity(0, Unit.COUNT);
+		this.records = new ArrayList<Transaction>();
+		this.orderPoint = op;
+	}
+	
+	public String getId() {
+		return this.storeId;
+	}
+	
+	public void addProductQuantity(Quantity q) {
+		this.productQuantity.add(q);
+	}
+	
+	public void removeProductQuantity(Quantity q) {
+		this.productQuantity.sub(q);
+	}
+	
+	public Quantity getProductQuantity() {
+		return this.productQuantity;
+	}
+	
+	public void addReservedQuantity(Quantity q) {
+		this.reservedQuantity.add(q);
+	}
+	
+	public void removeReservedQuantity(Quantity q) {
+		this.reservedQuantity.sub(q);
+	}
+	
+	public Quantity getReservedQuantity() {
+		return this.reservedQuantity;
+	}
+	
+	public ArrayList<Transaction> getRecords() {
+		return this.records;
+	}
+	
+	public void addRecord(Transaction t) {
+		this.records.add(t);
+	}
+	
+	public OrderPoint getOrderPoint() {
+		return this.orderPoint;
+	}
+	
+	public void setOrderPoint(OrderPoint op) {
+		this.orderPoint = op;
+	}
+	
+	public Product getProduct() {
+		return this.product;
+	}
+	
+	public void setProduct(Product p) {
+		this.product = p;
+	}
+
+	
 }
--- src/com/panthers/utilities/Quantity.java
+++ src/com/panthers/utilities/Quantity.java
@@ -1,7 +1,25 @@
 package com.panthers.utilities;
 
 public class Quantity {
-	private class Unit {
+	private int amount;
+	private Unit type;
+	public Quantity(int a, Unit t) {
+		this.amount = a;
+		this.type = t;
+		
+	}
+	
+	public void add(Quantity q) {
+		this.amount += q.getAmount();
+	}
+	
+	public void sub(Quantity q) {
+		this.amount -= q.getAmount();
+	}
+	
+	public int getAmount() {
+		return this.amount;
 		
 	}
 }
+
